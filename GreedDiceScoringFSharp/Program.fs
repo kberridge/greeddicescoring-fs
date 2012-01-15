1495f5b98fc8a1ad7c9484218f8c07f05a34719c
Kevin Berridge <kberridge@pointeblank.net>
2012-01-15 14:02:47 -0500

    F#!!!

---
+++ GreedDiceScoringFSharp/Program.fs
@@ -0,0 +1,44 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module GreedDiceScoring
+
+let incCount index ( counts : array<int>) =
+ counts.[index-1] <- counts.[index-1] + 1
+ counts
+
+let rec countWork dice (counts : array<int>) =
+  match dice with
+    | head :: tail -> countWork tail (incCount head counts)
+    | [] -> counts
+
+let count dice =
+  countWork dice [|0;0;0;0;0;0|]
+
+let rec scoreTuple x =
+  match x with
+  | (1, 1) -> 100
+  | (1, 3) -> 1000
+  | (x, 3) -> x * 100
+  | (5, 1) -> 50
+  | (x, 4) -> scoreTuple(x, 3) * 2
+  | (x, 5) -> scoreTuple(x, 3) * 4
+  | (x, 6) -> scoreTuple(x, 3) * 8
+  | _ -> 0
+
+let scoreCounts (c : array<int>) =
+  let numPairs = (Array.filter (fun e -> e = 2) c).Length
+  match numPairs with
+  | 3 -> 800
+  | _ -> 
+      match c with
+      | [|1;1;1;1;1;1|] -> 1200
+      | _ ->
+          scoreTuple (1, c.[0]) +
+          scoreTuple (2, c.[1]) + 
+          scoreTuple (3, c.[2]) +
+          scoreTuple (4, c.[3]) + 
+          scoreTuple (5, c.[4]) + 
+          scoreTuple (6, c.[5])
+
+let score dice =
+  scoreCounts (count dice)
---
+++ GreedDiceScoringTests/Tests.fs
@@ -0,0 +1,71 @@
+﻿module GreedDiceScoringTests
+
+open NaturalSpec
+open GreedDiceScoring
+
+let scored(dice) =
+  score(dice)
+
+[<Scenario>]
+let ``When a single 1 rolled``() =
+  Given [1] |> When scored |> It should equal 100 |> Verify
+
+[<Scenario>]
+let ``When a single 5 rolled``() =
+  Given [5] |> When scored |> It should equal 50 |> Verify
+
+[<Scenario>]
+let ``When triple 1s rolled``() =
+  Given [1;1;1] |> When scored |> It should equal 1000 |> Verify
+
+[<Scenario>]
+let ``When triple 2s rolled``() =
+  Given [2;2;2] |> When scored |> It should equal 200 |> Verify
+
+[<Scenario>]
+let ``When triple 3s rolled``() =
+  Given [3;3;3] |> When scored |> It should equal 300 |> Verify
+
+[<Scenario>]
+let ``When triple 4s rolled``() =
+  Given [4;4;4] |> When scored |> It should equal 400 |> Verify
+
+[<Scenario>]
+let ``When triple 5s rolled``() =
+  Given [5;5;5] |> When scored |> It should equal 500 |> Verify
+
+[<Scenario>]
+let ``When triple 6s rolled``() =
+  Given [6;6;6] |> When scored |> It should equal 600 |> Verify
+
+[<Scenario>]
+let ``When four 2s rolled``() =
+  Given [2;2;2;2] |> When scored |> It should equal 400 |> Verify
+
+[<Scenario>]
+let ``When four 1s rolled``() =
+  Given [1;1;1;1] |> When scored |> It should equal 2000 |> Verify
+
+[<Scenario>]
+let ``When four 6s rolled``() =
+  Given [6;6;6;6] |> When scored |> It should equal 1200 |> Verify
+
+[<Scenario>]
+let ``When three 1s and three 5s rolled``() =
+  Given [1;1;1;5;5;5] |> When scored |> It should equal 1500 |> Verify
+
+[<Scenario>]
+let ``When five 1s rolled``() =
+  Given [1;1;1;1;1] |> When scored |> It should equal 4000 |> Verify
+
+[<Scenario>]
+let ``When six 1s rolled``() =
+  Given [1;1;1;1;1;1] |> When scored |> It should equal 8000 |> Verify
+
+[<Scenario>]
+let ``When three pairs are rolled``() =
+  Given [2;2;3;3;4;4] |> When scored |> It should equal 800 |> Verify
+
+[<Scenario>]
+let ``When a straight is rolled``() =
+  Given [1;2;3;4;5;6] |> When scored |> It should equal 1200 |> Verify
